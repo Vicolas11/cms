2db25266c75ab7babeac8300c4754db353748532
Vicolas11 <incrediblechamp1@gmail.com>
2024-04-20 03:51:20 +0100

    feat: add auth and complaint layout

---
+++ components/AuthLayout/index.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react";
+import styles from "./styles.module.scss";
+import { Navbar } from "@/components/Navbar";
+
+export default function AuthLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className={styles.container}>
+      <Navbar />
+      <div className={styles.content}>{children}</div>
+      <div></div>
+    </div>
+  );
+}
