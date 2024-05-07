bad593b8d6b7b80b0516b94e22c4ffa26242bf03
Vicolas11 <incrediblechamp1@gmail.com>
2024-05-07 12:28:59 +0100

    chore: setup next.config for cloudinary images view

--- configs/constant.config.ts
+++ configs/constant.config.ts
@@ -1,4 +1,5 @@
 export const constant = {
-  devURL: process.env.DEV_URL,
-  prodURL: process.env.PROD_URL
+  prodDomain: process.env.PROD_DOMAIN,
+  prodURL: process.env.PROD_BASE_URL,
+  devURL: process.env.DEV_BASE_URL,  
 };
